8863d5be9760e60c6a1541b44a06cd9869a67272
Gronomix <Gronomix2013@gmail.com>
2023-04-03 15:00:44 +0300

    Решено 10 задание

--- task_10_matrix.py
+++ task_10_matrix.py
@@ -3,13 +3,13 @@
 
 from itertools import count as count_from
 
-count = count_from(1)
+count = count_from(2)
 matrix = [[next(count) for _ in range(3)] for _ in range(3)]
-print(matrix)
+print(f'Получили матрицу 3*3 {matrix}')
 
 for i in range(len(matrix)):
     for j in range(len(matrix[i])):
         if matrix[i][j] % 2 == 0:
             matrix[i][j] = 0
 
-print(matrix)
+print(f'заменили в исходной матрице все четные числа {matrix}')
